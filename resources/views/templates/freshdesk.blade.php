0f577dd3885b92c6abbfb8ef316edd9ba28d3437
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-03-19 10:48:00 +0000

    Add support for freshdesk

---
+++ resources/views/templates/freshdesk.blade.php
@@ -0,0 +1,3 @@
+<script type="text/javascript">
+	FreshWidget.init("", {"queryString": "&widgetType=popup&formTitle=Report+a+bug+or+issue&submitTitle=Report&submitThanks=Thank+you+for+your+feedback!+We+will+aim+to+get+back+to+you+within+the+next+couple+of+days.+If+your+query+is+urgent%2C+please+give+us+a+call.", "utf8": "✓", "widgetType": "popup", "buttonType": "text", "buttonText": "Report a Bug", "buttonColor": "white", "buttonBg": "#2452a1", "alignment": "4", "offset": "250px", "submitThanks": "Thank you for your feedback! We will aim to get back to you within the next couple of days. If your query is urgent, please give us a call.", "formHeight": "500px", "url": "https://bristolsu.freshdesk.com"} );
+</script>
