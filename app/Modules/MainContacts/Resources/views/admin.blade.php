5ad4281bf1f7e011f6d30a6710636c32b3465512
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-03-26 20:00:24 +0000

    Finished main contacts module

--- app/Modules/MainContacts/Resources/views/admin.blade.php
+++ app/Modules/MainContacts/Resources/views/admin.blade.php
@@ -8,8 +8,7 @@
             <div class="row" style="text-align: center;">
                 <div class="col-md-12">
                     <h2 class="">Main Contacts</h2>
-                    <p class="">This section is still in development. If you head to a group and click on their 'Main
-                        Contacts' module, you can see all the nominated contacts for that group.</p>
+                    <p class="">To see who is responsible for various things, head to the group page and click on their 'Main Contacts' module.</p>
 
                 </div>
             </div>
