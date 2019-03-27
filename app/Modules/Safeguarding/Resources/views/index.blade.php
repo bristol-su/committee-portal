930b5c494a22bb1d0c7ad89a559832b1c1d2b538
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-03-27 15:38:21 +0000

    Start on the safeguarding module

---
+++ app/Modules/Safeguarding/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('safeguarding::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('safeguarding.name') !!}
+    </p>
+@stop
---
+++ app/Modules/Safeguarding/Routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::prefix('strategicplan')->middleware(['user', 'module', 'module.active:strategicplan', 'module.maintenance:strategicplan'])->group(function() {
+//    Route::get('/', 'StrategicPlanController@showUserPage');
+//
+//    Route::FileUploads('StrategicPlanController');
+//
+//});
+//
+//Route::prefix('admin/strategicplan')->middleware(['admin', 'module', 'module.active:strategicplan', 'module.maintenance:strategicplan'])->group(function() {
+//    Route::get('/', 'StrategicPlanController@showAdminPage');
+//
+//    Route::get('/note-templates', 'StrategicPlanController@showNoteTemplatePage');
+//
+//    Route::FileUploadsAdmin('StrategicPlanController');
+//});
