d058b5051865a83632c833b138ee90a4572635ed
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-04-02 16:39:13 +0100

    Changes to feedback button

--- resources/views/templates/freshdesk.blade.php
+++ resources/views/templates/freshdesk.blade.php
@@ -1,14 +1,14 @@
 <script type="text/javascript">
     FreshWidget.init("", {
-        "queryString": "&widgetType=popup&formTitle=Report+a+bug+or+issue&submitTitle=Report&submitThanks=Thank+you+for+your+feedback!+We+will+aim+to+get+back+to+you+within+the+next+couple+of+days.+If+your+query+is+urgent%2C+please+give+us+a+call.",
+        "queryString": "&widgetType=popup&formTitle=Give+us+feedback+or+report+a+bug&submitTitle=Submit&submitThanks=Thank+you+for+your+feedback!+We+will+aim+to+get+back+to+you+within+the+next+couple+of+days.+If+your+query+is+urgent%2C+please+give+us+a+call.",
         "utf8": "✓",
         "widgetType": "popup",
         "buttonType": "text",
-        "buttonText": "Need a hand?",
+        "buttonText": "Feedback?",
         "buttonColor": "white",
         "buttonBg": "#2452a1",
         "alignment": "3",
-        "offset": "80%",
+        "offset": "60%",
         "submitThanks": "Thank you for your feedback! We will aim to get back to you within the next couple of days. If your query is urgent, please give us a call.",
         "formHeight": "500px",
         "url": "https://bristolsu.freshdesk.com"
