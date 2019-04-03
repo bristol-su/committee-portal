b711d749daf75039a01a366204b4aaab42b3afae
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-04-03 19:09:35 +0100

    Security patch
    Remove registration vunerability in which anyone could change the administrator user password (although coudn't get past email verification)

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -18,6 +18,8 @@
 
                         {{ __('Before proceeding, please check your email for a verification link.') }}
                         {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+
+
                     </div>
                 </div>
             </div>
