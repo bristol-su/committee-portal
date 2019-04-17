0efafd5486f2d4a697dc67face38b1ea1e10fd1f
Toby Twigger <tt15951@my.bristol.ac.uk>
2019-04-17 19:04:11 +0100

    Add sitewide back to portal button

---
+++ resources/views/templates/back_to_portal_button.blade.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12" style="text-align: right;">
+            <a href="{{homeURL()}}">
+                <button class="btn btn-info back-to-portal-btn">
+                    Back to Portal
+                </button>
+            </a>
+        </div>
+    </div>
+</div>
